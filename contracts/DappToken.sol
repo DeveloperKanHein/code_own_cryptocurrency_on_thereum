0a12812289d813c59ee2cea95982b3b4bef68318
DeveloperKanHein <developer.kanhein@gmail.com>
2023-08-15 15:44:42 +0630

    transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,33 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0.0";
     uint256 public totalSupply;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
     mapping(address => uint256) public balanceOf; 
 
     function DappToken(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        if(balanceOf[msg.sender] >= _value){
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -3,7 +3,20 @@ var DappToken = artifacts.require("./DappToken");
 contract('DappToken', function(accounts){
     var tokenInstance;
 
-    it('initialize the contact with the correct value', function(){})
+    it('initialize the contact with the correct value', function(){
+        return DappToken.deployed().then(function(instance ){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, "DApp Token", 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, 'DAPP', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, 'DApp Token v1.0.0', 'has the correct standard');
+        });
+    });
 
     it('allocate the initial supply upon deployment', function(){
         return DappToken.deployed().then(function(instance){
@@ -16,4 +29,27 @@ contract('DappToken', function(accounts){
             assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to admin account');
         });
     });
+
+
+    it('transfers token ownership', function(){
+        return DappToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 9999999999999999);
+        }).then(assert.fail).catch(function(error){
+            assert(true, 'error must contain revert');
+            // assert(error.message.indexOf('revert') >= 0, 'error must contain revert');
+            return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(receipt){
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 250000, 'add the amount to the receiving account');
+            return  tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+        });
+    });
+
 });
